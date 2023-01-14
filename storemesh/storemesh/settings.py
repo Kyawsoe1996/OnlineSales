80bda9331afcae9fc1085b92cdc4a0b8443390f5
Kyaw Soe <kssoe99@gmail.com>
2023-01-14 14:20:59 +0700

    Add Media and Static Configuration in settings and url

--- storemesh/storemesh/settings.py
+++ storemesh/storemesh/settings.py
@@ -120,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_in_venv/')
+
+#for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- storemesh/storemesh/urls.py
+++ storemesh/storemesh/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,9 @@ urlpatterns = [
 
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
