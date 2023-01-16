1f71372846aa7ac409357ba1a4c9c8ead038e168
Kyaw Soe <kssoe99@gmail.com>
2023-01-16 10:10:39 +0700

    Warehouse Temaple and View Logic Add

--- storemesh/inventory/urls.py
+++ storemesh/inventory/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path,include
+from .views import(
+        WarehouseListView,
+        WarehouseCreateView,
+        WarehouseDetailView,
+        WarehouseUpdateView,
+        WarehouseDeleteView,
+
+) 
+
 app_name="inventory"
 
 urlpatterns = [
     
-   
+    path("warehouse/list",WarehouseListView.as_view(),name="warehouse-list"),
+
+    path("warehouse/",WarehouseCreateView.as_view(),name="warehouse"),
+    path('warehouse/<int:pk>/', WarehouseDetailView.as_view(),name='warehouse-detail'),
+    path('warehouse/update/<int:pk>/', WarehouseUpdateView.as_view(),name='warehouse-update'),
+    path('warehouse/delete/<int:pk>/', WarehouseDeleteView.as_view(),name='warehouse-delete')
 ]
--- storemesh/inventory/views.py
+++ storemesh/inventory/views.py
@@ -1,3 +1,91 @@
 from django.shortcuts import render
+from django.http.request import HttpRequest
+from product.models import Product
+from django.http.response import HttpResponse, HttpResponsePermanentRedirect
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib import messages
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from inventory.models import Warehouse,Stock
+from django.utils import timezone
+from django.urls import reverse_lazy
+from django import forms
+import csv
 
-# Create your views here.
+class WarehouseListView(ListView):
+
+    model = Warehouse
+    # paginate_by = 100  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+class WarehouseCreateView(CreateView):
+    model = Warehouse
+    fields = ['name']
+    success_message = "Warehouse successfully added."
+    success_url = reverse_lazy('inventory:warehouse-list')
+
+
+    def get_form(self, form_class=None):
+        if form_class is None:
+            form_class = self.get_form_class()
+        form = super(WarehouseCreateView, self).get_form(form_class)
+        for i in form.fields:
+            form.fields[i].widget.attrs.update({'class':'form-control'})
+
+        form.fields['name'].widget = forms.TextInput(attrs={'placeholder': 'Enter name','class':'form-control'})
+        return form
+
+
+
+class WarehouseDetailView(DetailView):
+    model = Warehouse
+    template_name = "inventory/warehouse_detail.html"
+
+
+class WarehouseUpdateView(UpdateView):
+    model = Warehouse
+    fields = ['name']
+    success_url = reverse_lazy('inventory:warehouse-list')
+
+
+    def get_form(self, form_class=None):
+        if form_class is None:
+            form_class = self.get_form_class()
+        form = super(WarehouseUpdateView, self).get_form(form_class)
+        for i in form.fields:
+            form.fields[i].widget.attrs.update({'class':'form-control'})
+        form.fields['name'].widget = forms.TextInput(attrs={'placeholder': 'Enter name','class':'form-control'})
+        return form
+
+class WarehouseDeleteView(DeleteView):
+    model = Warehouse
+    success_url = reverse_lazy('inventory:warehouse-list')
+
+   
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
